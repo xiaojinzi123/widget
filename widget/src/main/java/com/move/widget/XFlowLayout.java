cc3d1e085581f6cf8262a6ba67d5a9ba697ed30d
xiaojinzi <347837667@qq.com>
2017-03-30 16:56:31 +0800

    添加了一个流式的布局的控件XFlowLayout

---
+++ widget/src/main/java/com/move/widget/XFlowLayout.java
@@ -0,0 +1,160 @@
+package com.move.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by cxj on 2017/3/30.
+ * 这是一个流式布局,但是并不是一个列表
+ * 如果想实现很长的列表的形式,对不起这个不适合你
+ * 流式布局这个可以放任何控件,但是每一个孩子高度都是一样的
+ * 关于宽度,此控件并没有处理测量,都是直接采用父容器推荐的宽,所以不能放在水平的列表或者水平的滚动ScrollerView中
+ * 关于高度,控件支持了包裹和填充父容器,也支持在竖直的列表和滚动ScrollerView中
+ */
+public class XFlowLayout extends ViewGroup {
+
+    public XFlowLayout(Context context) {
+        this(context, null);
+    }
+
+    public XFlowLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public XFlowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.XFlowLayout);
+
+        //获取横纵向的间距
+
+        mHSpace = a.getDimensionPixelSize(R.styleable.XFlowLayout_h_space, dpToPx(10));
+        mVSpace = a.getDimensionPixelSize(R.styleable.XFlowLayout_v_space, dpToPx(10));
+
+        a.recycle();
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+
+        int mWidthMeasureSpec = MeasureSpec.makeMeasureSpec(sizeWidth, MeasureSpec.AT_MOST);
+        int mHeightMeasureSpec = MeasureSpec.makeMeasureSpec(sizeHeight, MeasureSpec.AT_MOST);
+
+        //测量孩子的大小
+        measureChildren(mWidthMeasureSpec, mHeightMeasureSpec);
+
+        //寻找孩子中最高的一个孩子
+        findMaxChildMaxHeight();
+
+        int left = 0, top = 0;
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View view = getChildAt(i);
+
+            if (left != 0) { //是否是一行的开头
+                if ((left + view.getMeasuredWidth()) > sizeWidth) { //需要换行了,因为放不下啦
+                    //计算出下一行的top
+                    top += mChildMaxHeight + mVSpace;
+                    left = 0;
+                }
+            }
+
+            left += view.getMeasuredWidth() + mHSpace;
+
+        }
+
+        if (modeHeight == MeasureSpec.EXACTLY) {
+            //直接使用父类推荐的宽和高
+            setMeasuredDimension(sizeWidth, sizeHeight);
+        } else if (modeHeight == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(sizeWidth, (top + mChildMaxHeight) > sizeHeight ? sizeHeight : top + mChildMaxHeight);
+        } else {
+            setMeasuredDimension(sizeWidth, top + mChildMaxHeight);
+        }
+
+    }
+
+    /**
+     * 孩子中最高的一个
+     */
+    private int mChildMaxHeight;
+
+    /**
+     * 每一个孩子的左右的间距
+     */
+    private int mHSpace = 20;
+
+    /**
+     * 每一行的上下的间距
+     */
+    private int mVSpace = 20;
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+        findMaxChildMaxHeight();
+
+        //开始安排孩子的位置
+        int left = 0, top = 0;
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View view = getChildAt(i);
+
+            if (left != 0) { //是否是一行的开头
+                if ((left + view.getMeasuredWidth()) > getWidth()) { //需要换行了,因为放不下啦
+                    //计算出下一行的top
+                    top += mChildMaxHeight + mVSpace;
+                    left = 0;
+                }
+            }
+
+            //安排孩子的位置
+            view.layout(left, top, left + view.getMeasuredWidth(), top + mChildMaxHeight);
+
+            left += view.getMeasuredWidth() + mHSpace;
+
+        }
+
+    }
+
+    /**
+     * 寻找孩子中最高的一个孩子
+     */
+    private void findMaxChildMaxHeight() {
+        mChildMaxHeight = 0;
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View view = getChildAt(i);
+            if (view.getMeasuredHeight() > mChildMaxHeight) {
+                mChildMaxHeight = view.getMeasuredHeight();
+            }
+        }
+    }
+
+    /**
+     * dp的单位转换为px的
+     *
+     * @param dps
+     * @return
+     */
+    public int dpToPx(int dps) {
+        return Math.round(getResources().getDisplayMetrics().density * dps);
+    }
+
+    public void setHSpace(int hSpace) {
+        this.mHSpace = hSpace;
+    }
+
+    public void setVSpace(int vSpace) {
+        this.mVSpace = vSpace;
+    }
+}
